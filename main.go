2aad5f9c5d4e5c4893aeb299ccde60ef3f6386eb
Dirk Lembke <dirkl_propellerh@Dirks-MacBook-Pro.local>
2019-08-19 08:59:31 +1200

    check target-url and use httputil dump functions

--- main.go
+++ main.go
@@ -16,13 +16,20 @@ func main() {
 
 	flag.Parse()
 
+	if *target == "" {
+		panic(fmt.Errorf("empty target-url"))
+	}
+
 	remote, err := url.Parse(*target)
 	if err != nil {
 		panic(err)
 	}
 
+	log.Printf("Forward tarfic to %v", remote.String())
+
 	proxy := NewProxy(remote)
 	http.HandleFunc("/", handler(proxy))
+	log.Printf("Listen on %v", fmt.Sprintf(":%d", *port))
 	err = http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
 		panic(err)
@@ -34,18 +41,19 @@ func NewProxy(target *url.URL) *httputil.ReverseProxy {
 		req.URL.Scheme = target.Scheme
 		req.URL.Host = target.Host
 
-		//path := req.Header.Get("X-Original-URI")
-		//req.URL.Path = path
-		log.Println(req.Method + " " + req.URL.String())
-		txt := toJSON(req.Header)
-		log.Println(txt)
+		requestDump, err := httputil.DumpRequest(req, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(requestDump))
 	}
 
 	modify := func(res *http.Response) error {
-
-		log.Println(res.Status)
-		txt := toJSON(res.Header)
-		log.Println(txt)
+		resDump, err := httputil.DumpResponse(res, true)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(resDump))
 
 		if res.StatusCode > 200 && res.StatusCode < 300 {
 			res.StatusCode = 200
